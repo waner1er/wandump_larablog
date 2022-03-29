103c70abe620379bd8508162f3a731ecd722e737
waner1er <erwan@derniercri.io>
2022-03-29 12:33:05 +0200

    ckeditor-init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,33 @@
 <script src="https://cdn.ckeditor.com/ckeditor5/33.0.0/classic/ckeditor.js"></script>
 <script>
     ClassicEditor
-        .create( document.querySelector( '#editor' ) )
-        .then( editor => {
-            console.log( editor );
+        .create( document.querySelector( '#editor' ), {
+            toolbar: {
+                items: [
+                    'heading', '|',
+                    'alignment', '|',
+                    'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+                    'link', '|',
+                    'bulletedList', 'numberedList', 'todoList',
+                    'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor', '|',
+                    'code', 'codeBlock', '|',
+                    'insertTable', '|',
+                    'outdent', 'indent', '|',
+                    'uploadImage', 'blockQuote', '|',
+                    'undo', 'redo'
+                ],
+                shouldNotGroupWhenFull: true
+            },
+            heading: {
+                options: [
+                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+                ]
+            }
         } )
         .catch( error => {
-            console.error( error );
+            console.log( error );
         } );
 </script>
 </body>
